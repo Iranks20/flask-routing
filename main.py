7ae3a73c199518fb6d8190601eb6693f76b98c83
Iranks20 <rankunda48@gmail.com>
2023-01-17 13:14:21 +0300

    edited the navigation bar

--- main.py
+++ main.py
@@ -10,6 +10,34 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
+@app.route("/servicedetails")
+def servicedetails():
+    return render_template("service-details.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/blogdetails")
+def blogdetails():
+    return render_template("blog-details.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact-us.html")
+
 @app.route('/home/<name>')
 def welcome_name(name):
     # return 'Welcome ' + name + '!'
